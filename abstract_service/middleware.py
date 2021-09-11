2ef21ec184321f13dfb159c1259e8153e160bdfa
dk <ldifmo@gmail.com>
2021-09-11 19:47:04 +0300

    middleware exclude

--- abstract_service/middleware.py
+++ abstract_service/middleware.py
@@ -6,9 +6,11 @@ from abstract_service.settings import SECRET_TOKEN
 
 
 class SecretTokenMiddleware(BaseHTTPMiddleware):
+    path_exclude = ['/docs', '/openapi.json']
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        if request.headers.get("secretToken") != SECRET_TOKEN:
-            return JSONResponse(status_code=401)
+        if request.url.path not in self.path_exclude:
+            if request.headers.get("secretToken") != SECRET_TOKEN:
+                return JSONResponse(status_code=401)
         response = await call_next(request)
         return response
